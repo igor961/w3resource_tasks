4295b8dad457d87907b8ce7e99638ad6789d4301
Igor <igor.shesterikov2017@gmail.com>
2019-07-11 06:29:11 +0300

    Event Delegation Tasks

---
+++ learn_javascript_ru.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+  document.querySelector('#messages').addEventListener('click', e=>{
+    if (e.target.getAttribute('data-action')=='close') {
+      e.target.parentNode.style.display = 'none';
+    } 
+  })
+
+  Array.from(document.querySelectorAll('#messages>li')).map(el=>el.innerHTML+="<span data-action=\"close\" style=\"position: absolute; right: 5px; top: 5px;\">[x]</span>")
+
+  tree.addEventListener('click', e=>{
+    var el = e.target.nextSibling;
+    if (el.style.display == 'none') el.style.display = 'block'
+    else el.style.display = 'none'
+  })
+}
